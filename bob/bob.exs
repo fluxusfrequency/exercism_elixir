35a7c01515298fc50d8c78896787464b9cb7effe
Ben Lewis <bennlewis@gmail.com>
2014-10-23 20:35:40 -0600

    first stab at bob

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,7 +1,14 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-
+      String.strip(input) == "" ->
+        "Fine. Be that way!"
+      String.upcase(input) == input ->
+        "Woah, chill out!"
+      String.ends_with?(input, "?") ->
+        "Sure."
+      true ->
+        "Whatever."
     end
   end
 end
